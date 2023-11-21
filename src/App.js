76a225695c366fe5b3083c606b0d6480851afc8b
Olusola <olusolaadek@hotmail.com>
2023-11-21 13:26:34 -0700

    Created project setup

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { FaCalendarAlt, FaDoorOpen, FaUsers } from "react-icons/fa";
+
+import UserPicker from "./Users/UserPicker";
+import "./App.css";
+import BookingsPage from "./components/Bookings/BookingsPage";
+import BookablesPage from "./components/Bookables/BookablesPage";
+import UsersPage from "./components/Users/UsersPage";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/bookings" className="btn btn-header">
+                  <FaCalendarAlt />
+                  <span>Bookings</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/bookables" className="btn btn-header">
+                  <FaDoorOpen />
+                  <span>Bookables</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/users" className="btn btn-header">
+                  <FaUsers />
+                  <span>Users</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <UserPicker />
+        </header>
+
+        <Routes>
+          <Route path="/bookings" element={<BookingsPage />} />
+          <Route path="/bookables" element={<BookablesPage />} />
+          <Route path="/users" element={<UsersPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Bookables/BookablesPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BookablesPage() {
+  return <div>BookablesPage</div>;
+}
